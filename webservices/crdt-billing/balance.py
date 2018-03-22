2a963ed9dbcefb09f7d93c630e345534b664c278
Esther Jang <jang.esther@gmail.com>
2018-03-21 20:29:20 -0700

    added database updates, added balance check function

---
+++ webservices/crdt-billing/balance.py
@@ -0,0 +1,43 @@
+import MySQLdb
+import sys
+
+######### Get environment variables and args:
+
+
+########## Set up DB connection:
+
+
+def query_db(imsi):
+
+    #db = MySQLdb.connect(host="localhost",
+    #                    user="colte",
+    #                    passwd="correcthorsebatterystaple",
+    #		    db="crdt_db")
+    db = MySQLdb.connect(host="localhost",
+                        user="root",
+                        passwd="correcthorsebatterystaple",
+    		        db="crdt_db")
+    
+    cursor = db.cursor()
+    
+    ########### Perform DB Query:
+
+    query_str = "SELECT sum(amount) from `" + imsi + "`"
+    cursor.execute(query_str)
+    result = cursor.fetchone()
+    balance = "New balance for user with IMSI '" + imsi + "' is {}.".format(float(result[0]))
+
+    ########## Close out everything
+    
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return balance
+
+############ Main for testing
+
+if __name__ == "__main__":
+
+    imsi = sys.argv[1]
+    print query_db(imsi)
--- webservices/crdt-billing/test.sql
+++ webservices/crdt-billing/test.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE crdt_db;
 USE crdt_db;
 
-#DROP TABLE IF EXISTS `TEST`;
-CREATE TABLE IF NOT EXISTS `TEST` (
+DROP TABLE IF EXISTS `IMSITEST2`;
+CREATE TABLE `IMSITEST2` (
 	`timestamp` int(10) unsigned DEFAULT '0' NOT NULL,
 	`amount` int(11) DEFAULT '0' NOT NULL,
 	`user_imsi` varchar(16) NOT NULL,
-	`sender_id` varchar(16) NOT NULL,
+	`user_id` varchar(16) NOT NULL,
 	`bts_id` varchar(16) NOT NULL,
+	`ack` bit(1) DEFAULT 0 NOT NULL,
 	`sent` bit(1) DEFAULT 0 NOT NULL,
 	PRIMARY KEY (`timestamp`, `amount`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `TEST` VALUES (1, 100, 123451234512345, 'infrared', 2, 0);
+INSERT INTO `IMSITEST2` VALUES (1, 100, 'IMSI123451234512', 'matt9j', 2, 0, 0);
